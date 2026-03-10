587907e73f69bfb13de9238a227ed35a740aa152
Harry7986 <emarh.harry.code@gmail.com>
2026-03-10 23:55:26 +0100

    Form Validation Demo 3

--- src/components/FormValidationDemo.tsx
+++ src/components/FormValidationDemo.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/set-state-in-effect */
 import { useState, useEffect } from 'react';
 import { useDebounce } from '../hooks';
 import type { ValidationResult } from '../types';
