af8fda974316b1c6c1d6421557cee710c60a6b05
Harry7986 <emarh.harry.code@gmail.com>
2026-03-10 23:54:18 +0100

    Search Demo 6

--- src/components/SearchDemo.tsx
+++ src/components/SearchDemo.tsx
@@ -23,7 +23,6 @@ export const SearchDemo = () => {
 
   useEffect(() => {
     if (debouncedSearchTerm) {
-      // eslint-disable-next-line react-hooks/set-state-in-effect
       setSearchCount((prev) => prev + 1);
       
       // Simulate API call
